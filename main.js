37ccc26fb407285a5e2c8a2039828187e171ed02
nif <lnzhangsong@163.com>
2022-02-24 02:53:04 +0800

    feat(package): 增加打包配置

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const {
   app, //  控制应用生命周期的模块
   BrowserWindow // 创建原生浏览器窗口的模块
 } = require('electron')
-const path = require('path')
 const isDev = require('electron-is-dev');
 
 
@@ -16,7 +15,7 @@ function createWindow() {
   // 加载 页面内容
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${__dirname}/../build/index.html`);
   // 开启 开发者工具
-  mainWindow.webContents.openDevTools()
+  mainWindow.openDevTools()
   // win.loadURL('https://github.com')
 
   mainWindow.on('closed', () => {
