064912453752e712d20f73dedc466f92f6d18075
张松 <lnzhangsong@163.com>
2022-03-02 20:15:58 +0800

    feat(SideBar): initial completion of SideBar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,8 +1,36 @@
-function SideBar() {
+import { useState } from "react";
+
+import { Box, Progress, Tag, Avatar, Text } from "@chakra-ui/react";
+
+interface IProps {
+  used: number;
+  sum: number;
+}
+
+function SideBar(props: IProps) {
+  const { used, sum } = props;
+  const [rate] = useState(used / sum);
+
   return (
-    <div className="flex flex-col justify-between">
-      <header className="text-6xl">Top</header>
-      <footer className="text-6xl"></footer>
+    <div className="flex flex-col justify-between w-full">
+      <header className="">
+        <Box className="p-2 m-2 flex justify-around items-center border rounded-lg">
+          <Avatar bg="teal.500" />
+          <Text fontSize="4xl">LOGO</Text>
+        </Box>
+      </header>
+      <section className="h-full">
+        <Box className="h-full mx-2 p-2 flex justify-around items-center border rounded-lg"></Box>
+      </section>
+      <footer>
+        <Box className="p-3 h-20 m-2 flex flex-col justify-between border rounded-lg">
+          <div className="flex justify-between">
+            {" "}
+            <Tag>使用量 </Tag> {used} / {sum}{" "}
+          </div>
+          <Progress colorScheme="green" value={rate * 100} />
+        </Box>
+      </footer>
     </div>
   );
 }
--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -2,13 +2,11 @@ import { Outlet } from "react-router-dom";
 
 import SideBar from "../../components/SideBar/index";
 
-import "./index.scss";
-
 function Root() {
   return (
     <div className="flex">
-      <aside className="left_side flex flex-none">
-        <SideBar />
+      <aside className="left_side flex flex-none h-screen w-60">
+        <SideBar used={10} sum={20} />
       </aside>
       <div className="flex">
         <Outlet />
