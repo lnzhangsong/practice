13eea48f44ca9a1732b65701773dff2037afbd48
张松 <lnzhangsong@163.com>
2022-03-14 09:33:16 +0800

    feat(project): add eslint project

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,38 +2,36 @@ import { useEffect, useState } from "react";
 import { useRequest } from "ahooks";
 import { Tabs, TabPane } from "@douyinfe/semi-ui";
 
-import { retry as promiseRetry } from "@/utils/selfPromise";
+import { retry as promiseRetry } from "../../utils/selfPromise";
 
 import PageTable from "./components/PageTable";
 
 import { getList } from "../../api/buncket";
 
 function Home() {
-  const { data } = useRequest(() => promiseRetry(getList, 3, "网络错误"));
-  const [list, setList] = useState([]);
+	const { data } = useRequest(() => promiseRetry(getList, 3, "网络错误"));
+	const [list, setList] = useState([]);
 
-  useEffect(() => {
-    if (data) {
-      setList(data.data);
-    }
-  }, [data]);
-  return (
-    <>
-      <Tabs type="button" className="h-full flex flex-1 flex-col">
-        {list.map((item: any) => {
-          return (
-            <TabPane
-              key={item.Name}
-              tab={item.Name.substring(0, item.Name.indexOf("-"))}
-              itemKey={item.Name + item.CreationDate}
-            >
-              <PageTable key={item.Name + item.CreationDate} />
-            </TabPane>
-          );
-        })}
-      </Tabs>
-    </>
-  );
+	useEffect(() => {
+		if (data) {
+			setList(data.data);
+		}
+	}, [data]);
+	return (
+		<>
+			<Tabs type="button" className="h-full flex flex-1 flex-col">
+				{list.map((item: any) => (
+					<TabPane
+						key={item.Name}
+						tab={item.Name.substring(0, item.Name.indexOf("-"))}
+						itemKey={item.Name + item.CreationDate}
+					>
+						<PageTable key={item.Name + item.CreationDate} />
+					</TabPane>
+				))}
+			</Tabs>
+		</>
+	);
 }
 
 export default Home;
