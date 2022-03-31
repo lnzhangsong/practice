acb36b74991ca6ac446ff4f1bbe39f3067896883
nif <lnzhangsong@163.com>
2022-03-31 17:36:43 +0800

    refactor(project): update react to V18

--- server/router/objectRouter.ts
+++ server/router/objectRouter.ts
@@ -5,11 +5,14 @@ import router from "../utils/customRouter";
 import listObjects from "../src/object";
 
 router.get("/objectList", async (ctx: Context) => {
-  const { region, bunket } = ctx.request.query;
-  if (typeof region === "string") {
+  const { region, bunket, prefix } = ctx.request.query;
+  if (typeof region === "string"
+    && typeof bunket === "string"
+    && typeof prefix === "string") {
     const res = await listObjects({
       Region: region,
       Bunket: bunket,
+      Prefix: prefix,
     });
     ctx.body = res;
   }
--- server/src/object.ts
+++ server/src/object.ts
@@ -5,6 +5,11 @@ import {
   IListObjectResponce,
 } from "./types/objectTypes";
 
+/**
+ * 获取文件夹下的内容
+ * @param request 条件
+ * @returns 文件夹内容
+ */
 const listObjects = (request: IListObjectRequest): Promise<IListObjectResponce> => {
   return new Promise((resolve, reject) => {
     cos.getBucket({ request }, (err: IListObjectResponce, data: IListObjectResponce) => {
